090bb71e130de362dbf241ba0586238b8c685927
B3rX <ed.esteban.ruiz@gmail.com>
2024-05-26 01:18:18 -0500

    Updated CrossOrigin

--- TurnsService/src/main/java/edu/uptc/swii/TurnsService/controller/TurnsController.java
+++ TurnsService/src/main/java/edu/uptc/swii/TurnsService/controller/TurnsController.java
@@ -5,11 +5,13 @@ import edu.uptc.swii.TurnsService.service.TurnService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/turns")
 public class TurnsController {
     @Autowired
